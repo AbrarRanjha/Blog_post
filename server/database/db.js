cedc6584adc1965a95cdd5774b8da40a5d502243
ibrar-techvention <ibrar.hussain@techvention.ae>
2023-05-28 14:50:00 +0500

    updated

--- server/database/db.js
+++ server/database/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 
 const Connection = async (username, password) => {
-    const URL = `mongodb+srv://${username}:${password}@cluster0.67rft.mongodb.net/BLOG?retryWrites=true&w=majority`;
+    const URL = `mongodb://127.0.0.1:27017/Blog_post`;
 
     try {
         await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false ,useCreateIndex: true})
